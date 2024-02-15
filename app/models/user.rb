e01c42b16424cb1948209862cd80aa79d7795924
Tyler Fenton <tsfento@gmail.com>
2024-02-15 15:10:14 -0600

    Created user tests and a user model to pass them

---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+class User < ApplicationRecord
+    has_secure_password
+
+    validates :username, presence: true, uniqueness: true, length: { minimum: 5, maximum: 15 }
+    validate :validate_username
+    validates :email, presence: true, uniqueness: true, length: { minimum: 7, maximum: 254 },
+    format: {
+        with: URI::MailTo::EMAIL_REGEXP
+    }
+
+    validates :password, presence: true, length: { minimum: 8 },
+    format: {
+        with: /\A.*(?=.*\d)(?=.*[!@#$%^&*]).*\z/,
+        message: 'must contain at least one digit and one special character'
+    }, on: :create
+    validates :password_confirmation, presence: true, length: { minimum: 8 }, on: :create
+
+    private
+
+    def validate_username
+        unless username =~ /\A[a-zA-Z0-9_]+\z/
+            errors.add(:username, 'can only contain letters, numbers and underscores and must include at least one letter or number')
+        end
+    end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,107 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  context 'validation' do
+    it 'is not valid without a username' do
+      user = build(:user, username: nil)
+      expect(user).not_to be_valid
+    end
+
+    it 'is not valid with a username with less than 5 characters' do
+      user = build(:user, username: 'bob')
+      expect(user).not_to be_valid
+    end
+
+    it 'is not valid with a username with more than 15 characters' do
+      user = build(:user, username: 'abcdefghijklmnop')
+      expect(user).not_to be_valid
+    end
+
+    it 'is not valid with a username with invalid characters' do
+      user = build(:user, username: 'jAk0b!')
+      expect(user).not_to be_valid
+    end
+
+    it 'is not valid without an email' do
+      user = build(:user, email: nil)
+      expect(user).not_to be_valid
+    end
+
+    it 'is not valid with an email with less than 7 characters' do
+      user = build(:user, email: '@.com')
+      expect(user).not_to be_valid
+    end
+
+    it 'is not valid with an email with more than 254 characters' do
+      user = build(:user, email: 'Q5yciROpzU9aI7kpkqLsO5IECsnG2DjJ
+        rj1OvA4miFKYGF3SI6lK7aqdLzRhgZAG
+        QcLjyvLCvj4RV7mEpt4XmumTgLkrZ9jD
+        3gqfL9X2zV9nVrTV9BR0hTVcGEzehL2x@
+        0rNFCVh9QO40UHhoWASsqRJLo2dny322
+        8IpSHklaASVl5X8Ot9803nTFUWhNv1HY
+        wSBhjfkiWxcXyGeC6n8Gzwcuo5nQmQt8
+        vvjzgj2Ae8WP3Aq4bkjCYMhjELohuiFw.com')
+      expect(user).not_to be_valid
+    end
+
+    it 'is not valid without a password' do
+      user = build(:user, password: nil)
+      expect(user).not_to be_valid
+    end
+
+    it 'is not valid with a password without a special character' do
+      user = build(:user, password: 'jAk0bWuZhErE')
+      expect(user).not_to be_valid
+    end
+
+    it 'is not valid with a password with invalid characters' do
+      user = build(:user, password: 'jAk0bWuZhErE/')
+      expect(user).not_to be_valid
+    end
+
+    it 'is not valid without password_confirmation' do
+      user = build(:user, password_confirmation: nil)
+      expect(user).not_to be_valid
+    end
+
+    it 'hashes the password' do
+      user = create(:user)
+      expect(user.password_digest).not_to eq('password')
+    end
+  end
+
+  context 'uniquness' do
+    it 'is not valid without a unique username' do
+      user1 = create(:user)
+      user2 = build(:user, username: user1.username)
+      expect(user2).not_to be_valid
+      expect(user2.errors[:username]).to include('has already been taken')
+    end
+
+    it 'is not valid without a unique email' do
+      user1 = create(:user)
+      user2 = build(:user, email: user1.email)
+      expect(user2).not_to be_valid
+      expect(user2.errors[:email]).to include('has already been taken')
+    end
+  end
+
+  context 'dependency destruction' do
+    let(:user) { create(:user) }
+    let(:user_id) { user.id }
+
+    before { user.destroy }
+
+    it 'deletes profile' do
+    end
+
+    it 'deletes watch_lists' do
+    end
+
+    it 'deletes user_watch_titles' do
+    end
+
+    it 'deletes watched_dates' do
+    end
+  end
+end
