ffb7fe1c5d60782f784282882b08aa18cfea6f7d
Tyler Fenton <tsfento@gmail.com>
2024-02-26 13:14:07 -0600

    Barebones authentication done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,13 @@
 class SessionsController < ApplicationController
     def create
         user = User.find_by(username: params[:username])
-    end
 
-    if user&.authenticate(params[:password])
-        token = jwt_encode(user_id: user.id)
-        render json: { token: token }, status: :ok
-    else
-        render json: { error: 'unauthorized' }, status: :unauthorized
+        if user&.authenticate(params[:password])
+            token = jwt_encode(user_id: user.id)
+            render json: { token: token }, status: :ok
+        else
+            render json: { error: 'unauthorized' }, status: :unauthorized
+        end
     end
 
     private
