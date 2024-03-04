e1a696d7f9afc87451b7e23f31d62c1e6de22a68
Tyler Fenton <tsfento@gmail.com>
2024-03-04 15:31:26 -0600

    Finished creating a new list on a user

--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,7 @@ Rails.application.routes.draw do
     # get all lists of a specific user
     get '/:username/lists', to: 'users#lists_index'
     # create a new list for a specific user
-    post '/:username/lists', to: 'watch_lists#create_list'
+    post '/:username/lists', to: 'watch_lists#create'
     # get specific list from specific user
     get '/:username/lists/:id', to: 'users#titles_show'
     # get a specific user's followed lists
