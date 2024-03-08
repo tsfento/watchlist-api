b28429afdfa477d65bf081871bb64826cbdbe2f6
Tyler Fenton <tsfento@gmail.com>
2024-03-08 15:35:21 -0600

    Troubleshooting missing secret_key_base

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,6 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.secret_key_base = Rails.application.credentials.secret_key_base
 
   # Code is not reloaded between requests.
   config.enable_reloading = false
